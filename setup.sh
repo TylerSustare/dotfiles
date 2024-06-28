ecd584e3e6e5ac46cec72b297f284e5ab02a338b
Tyler Sustare <tyler.sustare@shopify.com>
2024-06-28 14:21:30 -0700

    Add YADR

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 # ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
